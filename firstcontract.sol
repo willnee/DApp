7140640f69345eb4fd281099111b09e64dcabc19
willnee <luongcongtien111999@gmail.com>
2022-02-02 22:02:33 +0700

    modifier

--- firstcontract.sol
+++ firstcontract.sol
@@ -1,13 +1,33 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-contract firstcontract {
-    uint saveData;
+contract Firstcoin {
+    address public minter;
+    mapping (address => uint) public balances;
+    event sent(address from,address to,uint amount);
 
-    function set(uint x) public {
-        saveData = x;
-
+    modifier onlyMinter {
+         require(msg.sender == minter);
+         _;
     }
-    function get()public view returns (uint x) {
-        return saveData;
+    modifier checkamount(uint amount) {
+        require(amount <1e60);
+        _;
+    }
+    modifier checkbalance(uint amount ){
+        require(amount<=balances[msg.sender],"Khong du tien");
+        _;
+    }
+
+    constructor (){
+        minter= msg.sender;
+    }
+    function mint(address receiver,uint amount)public  onlyMinter checkamount(amount){
+        balances[receiver] +=amount;
+    
+    }
+    function send(address receiver,uint amount)public checkbalance(amount){
+        balances[msg.sender] -=amount;
+        balances[receiver] += amount;
+        emit sent(msg.sender,receiver,amount);
     }
 }
